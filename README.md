c11c49ba93ca101cd0df366e91cf442f28d31236
bench <bench@LS40>
2024-02-14 12:06:29 +0900

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## File Redirection
+
+Redirection of File
+
+#### License
+
+MIT
---
+++ file_redirection/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "File Redirection",
+			"type": "module",
+			"label": _("File Redirection")
+		}
+	]
